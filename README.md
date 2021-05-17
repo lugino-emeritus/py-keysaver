e6a9ca087eb3480de605fa0243c0a137972f6029
lugino-emeritus <lugino-emeritus@users.noreply.github.com>
2021-05-17 12:37:03 +0200

    generic update
    keysaver 0.2 removed

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # Python KeySaver
+
 This is a small tool to create random passwords and save them encrypted.
 A master password is used to encrypt the data.
 
 
-### New in Version 0.3.0:
-Complete redesign. To convert the old password file open the old keysaver `python -i keysaver_0-2-2.py`, then call `convert_pw_dic()`. **keysaver_0-2-2.py will be removed in the next update!**
-
-
 #### Dependencies:
+
 - python 3.6 or newer
 - argon2 `pip install argon2-cffi`
 - msgpack `pip install msgpack`
@@ -17,21 +15,23 @@ Complete redesign. To convert the old password file open the old keysaver `pytho
 
 
 ## Usage
-Start the script with `python -i keysaver.py`. It will create an encrypted file `pwdic` for your future passwords. To change the filename edit the `FILENAME` constant in keysaver.py
 
-After 5 minutes without interaction it is necessary to enter the master password again.
+Start the script with `python -i keysaver.py [<pwdic>]`. It will open or create an encrypted file `<pwdic>` where your passwords will be stored.
+
+After 5 minutes without interaction you have to enter the master password again.
 
 
-### Commands:
+### Commands
+
 The following commands are probably self-explanatory. `name` is the name of a password-line.
 
 - `add_pw_line(name)`
 - `delete_pw_line(name)`
 - `copy_pw_line(name)`: duplicate the pw_line
 - `move_pw_line(name)`: rename the pw_line
-- `edit_pw_line(name)`
+- `edit_pw_line(name)`: modify additional informations
 - `list_pw_lines(keys=('description', 'username'))`: shows names and info for all saved password-lines. `keys=None` will show all keywords.
-- `pw_info(name)`: show all information assigned to the `pw_line` by `edit_pw_line`
+- `pw_info(name)`: show all information assigned to `name`
 - `show_pw(name, info=False)`
 - `copy_pw(name, info=False)`
 - `change_pw(name)`
@@ -39,12 +39,12 @@ The following commands are probably self-explanatory. `name` is the name of a pa
 To change the master password call `change_mpw()`.
 
 
-### Some details
+### Some technical details
 
-The file is encrypted with AES256 GCM if using method `\x00\x01` (the default). Method `\x00\x00` does not encrpyt and is only for testing.
+The file is encrypted with AES256 GCM if using method `\x00\x01` (default setting). Method `\x00\x00` does not encrypt and is only for debugging.
 
 To expand the master password to the AES key argon2id is used (512 MiB RAM). The master password is never saved.
 
-Moreover each password itself is encrypted using AES CBC and a token derived by the master password. After a random value of 400 to 600 days without changing a password you will be asked if you want to change it.
+Moreover each password itself is encrypted using AES CBC and a token derived from the master password. After a random value of 400 to 600 days without changing a password you will be asked whether you want to change it.
 
-Each time you open and save the file (automatically done by e.g. adding or changing a pw) it uses a new salt.
+Each time you open and save the file (automatically done by e.g. adding or changing a password) a new salt is created.
--- keysaver.py
+++ keysaver.py
@@ -2,24 +2,24 @@
 file syntax:
 	- version (2 byte), b'\x00\x01'
 	- encryption method (2 bytes):
-	  b'\x00\x01':
-	  - argon2 to expand pw to 32 byte key with 32 byte salt
-	  - AES GCM global encryption with 12 byte nonce
-	  - AES CBC for passwords: 16 byte iv, token as key, no MAC
-	  - encrypted data:
-	  	- 32 byte salt to derive a token = sha256(salt + pw)
-	  	- dictionary stored as msgpack
-		- MAC of AES GCM with aad over version, method and argon2 salt
+		b'\x00\x01':
+			- argon2id to expand pw to 32 byte key with 32 byte salt
+			- 32 byte salt to derive a master token = sha256(salt + pw)
+			- AES GCM global encryption with 12 byte nonce
+			- AES CBC for passwords: 16 byte iv, token as key, no MAC
+	- dictionary stored as msgpack
+	- MAC of AES GCM with aad over version, method and salt
 
-pw_dic['name'] = {'info': {"description": "Mail Telekom Main", "username": "username"},
-                       'update_ts': 1234567890,
-                       'enc_data': b'salt and encrypted password'}
+pwdic['name'] = {'info': {"description": "a description", "username": "user"},
+                  'update_ts': 1234567890,
+                  'enc_data': b'salt and encrypted password'}
 '''
 import argon2
 import datetime
 import hashlib
 import msgpack
 import os
+import sys
 import pyperclip
 import time
 
@@ -36,11 +36,11 @@ from tabulate import tabulate
 from ntlib.fctthread import ThreadLoop
 
 __author__ = 'NTI (lugino-emeritus) <*@*.de>'
-__version__ = '0.3.9'
+__version__ = '0.3.12'
 
 FILENAME = "pwdic"
 PW_DEFAULT_LEN = 12
-MASTER_TOKEN_TIME = 300
+TOKEN_EXPIRATION = 300
 
 _VERSION = b'\x00\x01'
 _ENC_METHOD = b'\x00\x01'
@@ -55,7 +55,7 @@ def _utc_msts48():
 sys_randint = _SystemRandom().randint  # returns n with a <= n <= b
 
 def pw_lifetime():
-	return 86400 * sys_randint(400, 600)
+	return sys_randint(400*86400, 600*86400)
 
 _salt_count = sys_randint(0, 2**32-1)
 def gen_salt(n):
@@ -77,7 +77,7 @@ def _mix_list(x):
 		(x[i], x[j]) = (x[j], x[i])
 	return x
 
-def gen_rand_pw(n=PW_DEFAULT_LEN, *, symbols=_RAND_SYMBOLS):
+def gen_rand_pw(n=PW_DEFAULT_LEN, symbols=_RAND_SYMBOLS):
 	chars = _RAND_CHARS + symbols
 	m = len(chars) - 1
 	if n < 6:
@@ -106,9 +106,12 @@ def _yes_no_question(s):
 
 def _read_new_pw():
 	pw = getpass('Enter new password: ')
-	while getpass('Repeat password: ') != pw:
-		pw = getpass('Passwords are not equal, try again: ')
-	return pw.encode()
+	while True:
+		while not pw:
+			pw = getpass('Choose a longer password: ')
+		if getpass('Repeat password: ') == pw:
+			return pw.encode()
+		pw = getpass('Passwords does not match, try again: ')
 
 def _read_rand_pw():
 	data = input(f'To generate random password enter length > 0, 1 means default length ({PW_DEFAULT_LEN}): ')
@@ -157,7 +160,7 @@ class crypto:
 #-------------------------------------------------------
 
 class DicSaver:
-	def __init__(self, filename=FILENAME):
+	def __init__(self, filename):
 		self.filename = filename
 		self._method = None
 		self._enc_salt = None
@@ -165,6 +168,9 @@ class DicSaver:
 		self._token_data = None
 		self._token = None
 
+	@property
+	def method(self):
+		return self._method
 
 	def _refresh_pw(self, pw):
 		if self._method == b'\x00\x00':
@@ -191,6 +197,14 @@ class DicSaver:
 			self._token = hashlib.sha256(self._token_data + pw).digest()
 		else:
 			raise KeyError(f'method {self._method} unknown')
+
+	def get_token(self):
+		if self._token is None:
+			pw = getpass('Enter master password: ').encode()
+			while not self._check_pw(pw):
+				pw = getpass('Wrong password, try again: ').encode()
+			self._set_token(pw)
+		return self._token
 
 
 	def read(self):
@@ -236,11 +250,6 @@ class DicSaver:
 		with open(self.filename, 'wb') as f:
 			f.write(aad + data)
 
-
-	@property
-	def method(self):
-		return self._method or b'\x00\x00'
-
 	def change_pw(self, *, method=None):
 		if self._enc_key:
 			pw = getpass('Enter current master password: ').encode()
@@ -266,17 +275,9 @@ class DicSaver:
 		self._set_token(pw)
 		return self._token, old_token
 
-	def get_token(self):
-		if self._token is None:
-			pw = getpass('Enter master password: ').encode()
-			while not self._check_pw(pw):
-				pw = getpass('Wrong password, try again: ').encode()
-			self._set_token(pw)
-		return self._token
-
 
 class TokenDicSaver(DicSaver):
-	def __init__(self, filename=FILENAME):
+	def __init__(self, filename):
 		super().__init__(filename)
 		self._alive_ts = 0
 		self._loop_ctl = ThreadLoop(self._loop)
@@ -293,12 +294,12 @@ class TokenDicSaver(DicSaver):
 
 	def _set_token(self, pw):
 		super()._set_token(pw)
-		self._alive_ts = time.time() + MASTER_TOKEN_TIME
+		self._alive_ts = time.time() + TOKEN_EXPIRATION
 		self._loop_ctl.start()
 
 	def get_token(self):
 		if self._loop_ctl.is_alive():
-			ts = time.time() + MASTER_TOKEN_TIME
+			ts = time.time() + TOKEN_EXPIRATION
 			if ts > self._alive_ts:
 				self._alive_ts = ts
 		elif self._token is not None:
@@ -325,18 +326,12 @@ def _decrypt_data(token, data, *, method):
 
 #-------------------------------------------------------
 
-def _alt_file(filename):
-	return filename if os.path.exists(filename) else os.path.join(os.path.dirname(__file__), filename)
-
-dic_saver = TokenDicSaver(_alt_file(FILENAME))
-pw_dic = None
-
 def read_dic():
-	global pw_dic
-	pw_dic = dic_saver.read()
+	global pwdic
+	pwdic = dic_saver.read()
 def save_dic():
-	global pw_dic
-	dic_saver.save(pw_dic)
+	global pwdic
+	dic_saver.save(pwdic)
 
 def get_token():
 	return dic_saver.get_token()
@@ -345,24 +340,24 @@ def change_mpw(method=_ENC_METHOD):
 	old_method = dic_saver.method
 	token, old_token = dic_saver.change_pw(method=method)
 	method = dic_saver.method
-	for name in pw_dic:
-		pw = _decrypt_data(old_token, pw_dic[name]['enc_data'], method=old_method)
-		pw_dic[name]['enc_data'] = _encrypt_data(token, pw, method=method)
+	for name in pwdic:
+		pw = _decrypt_data(old_token, pwdic[name]['enc_data'], method=old_method)
+		pwdic[name]['enc_data'] = _encrypt_data(token, pw, method=method)
 	save_dic()
 
 #-------------------------------------------------------
 
 def set_pw(name, pw):
 	#pw must be a byte-like object
-	pw_dic[name]['enc_data'] = _encrypt_data(dic_saver.get_token(), pw, method=dic_saver.method)
-	pw_dic[name]['update_ts'] = int(utc_ts()) + pw_lifetime()
+	pwdic[name]['enc_data'] = _encrypt_data(dic_saver.get_token(), pw, method=dic_saver.method)
+	pwdic[name]['update_ts'] = int(utc_ts()) + pw_lifetime()
 	save_dic()
 def get_pw(name):
 	#return pw as string
-	return _decrypt_data(dic_saver.get_token(), pw_dic[name]['enc_data'], method=dic_saver.method).decode()
+	return _decrypt_data(dic_saver.get_token(), pwdic[name]['enc_data'], method=dic_saver.method).decode()
 
 def pw_info(name):
-	print(tabulate(sorted(pw_dic[name]['info'].items())))
+	print(tabulate(sorted(pwdic[name]['info'].items())))
 
 def show_pw(name, info=False):
 
@@ -371,7 +366,7 @@ def show_pw(name, info=False):
 		pw_info(name)
 		print(f'password: {pw}')
 	else:
-		print(f"username: {pw_dic[name]['info']['username']}, password: {pw}")
+		print(f"username: {pwdic[name]['info']['username']}, password: {pw}")
 
 def copy_pw(name, info=False):
 	pyperclip.copy(get_pw(name))
@@ -379,7 +374,7 @@ def copy_pw(name, info=False):
 		pw_info(name)
 		print('password copied')
 	else:
-		print(f"username: {pw_dic[name]['info']['username']}, password copied")
+		print(f"username: {pwdic[name]['info']['username']}, password copied")
 
 def change_pw(name):
 	if _yes_no_question('Show current password? '):
@@ -389,7 +384,7 @@ def change_pw(name):
 
 
 def add_pw_line(name):
-	if name in pw_dic and not _yes_no_question('Name already in pw_dic, overwrite? '):
+	if name in pwdic and not _yes_no_question('Name already exists, overwrite? '):
 		return
 	pw_info = {}
 	pw_info['username'] = input('Username: ')
@@ -399,33 +394,33 @@ def add_pw_line(name):
 	while _yes_no_question('Do you want to add additional information? '):
 		x = input('Enter name of new key: ')
 		pw_info[x] = input(f'Enter {x}: ')
-	pw_dic[name] = {'info': pw_info}
+	pwdic[name] = {'info': pw_info}
 	set_pw(name, pw)
 
 def delete_pw_line(name):
 	pw_info(name)
 	if _yes_no_question(f'Do you really want to delete {name}? '):
-		del pw_dic[name]
+		del pwdic[name]
 		save_dic()
 
 def copy_pw_line(name):
 	new_name = input('Enter new name: ')
-	if new_name in pw_dic:
-		if not _yes_no_question('Name already in pw_dic, overwrite? '):
+	if new_name in pwdic:
+		if not _yes_no_question('Name already exists, overwrite? '):
 			return
-	pw_dic[new_name] = pw_dic[name].copy()
+	pwdic[new_name] = pwdic[name].copy()
 	save_dic()
 
 def move_pw_line(name):
 	new_name = input('Enter new name: ')
-	if new_name in pw_dic and not _yes_no_question('Name already in pw_dic, overwrite? '):
+	if new_name in pwdic and not _yes_no_question('Name already exists, overwrite? '):
 		return
-	pw_dic[new_name] = pw_dic.pop(name)
+	pwdic[new_name] = pwdic.pop(name)
 	save_dic()
 
 def edit_pw_line(name):
 	pw_info(name)
-	info = pw_dic[name]['info']
+	info = pwdic[name]['info']
 	while True:
 		x = input('Enter name of (new) key: ')
 		if not x:
@@ -446,32 +441,34 @@ def list_pw_lines(keys=('description', 'username')):
 	To show all available info call list_pw_lines(None)
 	'''
 	if not keys:
-		keys = sorted(set(k for v in pw_dic.values() for k in v['info']))
+		keys = sorted(set(k for v in pwdic.values() for k in v['info']))
 	elif isinstance(keys, str):
 		keys = (keys,)
 	arr = tuple((name,) + tuple(val['info'].get(k, '') for k in keys)
-			for name, val in sorted(pw_dic.items()))
+			for name, val in sorted(pwdic.items()))
 	print(tabulate(arr, headers=(('name',) + tuple(keys))))
 
 
 def check_lifetimes():
 	now = int(utc_ts())
-	to_update = tuple(name for name, v in pw_dic.items() if v['update_ts'] < now)
+	to_update = tuple(name for name, v in pwdic.items() if v['update_ts'] < now)
 	if not (to_update and _yes_no_question('There are passwords to renew. Renew them now? ')):
 		return
 	for name in to_update:
 		if _yes_no_question(f'The lifetime of {name} expired. Do you want to change the password? '):
 			change_pw(name)
 		elif _yes_no_question('Do you want to extend the lifetime? '):
-			pw_dic[name]['update_ts'] = now + pw_lifetime()
+			pwdic[name]['update_ts'] = now + pw_lifetime()
 			save_dic()
 
 #-------------------------------------------------------
 
 if __name__ == '__main__':
-	if os.path.exists(FILENAME):
+	filename = sys.argv[1] if len(sys.argv) > 1 else FILENAME
+	dic_saver = TokenDicSaver(filename)
+	if os.path.exists(filename):
 		read_dic()
 		check_lifetimes()
-	elif _yes_no_question(f"File '{FILENAME}' not found, create a new password file? "):
-		pw_dic = {}
+	elif _yes_no_question(f"File '{filename}' not found, create a new password file? "):
+		pwdic = {}
 		change_mpw()
